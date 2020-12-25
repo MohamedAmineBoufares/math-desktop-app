1f8d59f3091403ddf8f14d2041360b392aebf979
Mohamed Amine Boufares <medamineboufares@gmail.com>
2020-12-25 18:59:47 +0100

    Add files via upload
    Added Dérivé and Integration function to the code, now the GUI calculate the Dérivé and the Intégrale of the main function, and it also displays the 3 functions

--- tk_gui_V0.py
+++ tk_gui_V0.py
@@ -1,4 +1,3 @@
-
 import matplotlib
 matplotlib.use('TkAgg')
 import numpy as np
@@ -6,11 +5,15 @@ from numpy import sin ,cos,exp,log,sqrt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from tkinter import *
+from sympy import *
+
+
 class mclass:
+
     def __init__(self,  window):
         self.window = window
-        self.fr1 = Frame(window,highlightbackground="gray", highlightthickness=2, width=100, height=100, bd= 5)
-        self.fr2 = Frame(window,highlightbackground="darkgray", highlightthickness=2, width=100, height=100, bd= 5)
+        self.fr1 = Frame(window,highlightbackground="black", highlightthickness=2, width=100, height=100, bd= 5) # à gauche
+        self.fr2 = Frame(window,highlightbackground="darkgray", highlightthickness=2, width=100, height=100, bd= 5) # à droite
         self.func_txt=StringVar()
         self.func_txt.set("L'expression de f :")
         self.label_func=Label(self.fr1, textvariable=self.func_txt,justify=RIGHT, height=4)
@@ -27,26 +30,61 @@ class mclass:
         self.label_b.grid(sticky = E,row=3,column=0)
         self.boxb = Entry(self.fr1,width=10,borderwidth=3,bg="powder blue")
         self.boxb.grid(sticky = W,row=3,column=1)
-        self.box = Entry(self.fr1,borderwidth=3,bg="powder blue")
+        self.box = Entry(self.fr1,borderwidth=3,bg="white")
         self.box.grid(row=1,column=1)
+
+        # Dérivé
+        self.diriv_txt=StringVar()
+        self.diriv_txt.set("Dérivé: ")
+        self.label_diriv = Label(self.fr1, textvariable=self.diriv_txt,justify=RIGHT, anchor="w", height=4)
+        self.label_diriv.grid(sticky = E, row=4, column=0)
+        
+        self.res_txt=StringVar()
+        self.res_diriv = Label(self.fr1,textvariable=self.res_txt,justify=RIGHT, anchor="w", width=10,borderwidth=3,bg="powder blue")
+        self.res_diriv.grid(sticky = W, row=4, column=1)
+
+        # Prémitive
+        self.pri_txt=StringVar()
+        self.pri_txt.set("Primitive: ")
+        self.label_pri = Label(self.fr1, textvariable=self.pri_txt,justify=RIGHT, anchor="w")
+        self.label_pri.grid(sticky = E, row=5, column=0)
+        
+        self.resp_txt=StringVar()
+        self.res_prim = Label(self.fr1,textvariable=self.resp_txt,justify=RIGHT, anchor="w", width=10,borderwidth=3,bg="powder blue")
+        self.res_prim.grid(sticky = W, row=5, column=1)
+        
+        # Button
         self.button = Button (self.fr1, width =35,text="plot",bg="powder blue", command=self.plot)
-        self.button.grid(row=4,column=0,columnspan=3)
+        self.button.grid(row=6,column=0,columnspan=3)
         self.fr1.grid(row=1,column=0,padx=10,pady=10,sticky="ns")
         self.fr2.grid(row=1,column=1,padx=10,pady=10)
+           
+        # Figure
         self.fig = Figure(figsize=(6,6))
         self.a = self.fig.add_subplot(111)
         self.a.set_title ("Graphe de f", fontsize=16)
         self.a.set_ylabel("y=f(x)", fontsize=14)
         self.a.set_xlabel("x", fontsize=14)
-        self.a.set_facecolor("gray")
+        self.a.set_facecolor("black")
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.fr2)
         self.canvas.get_tk_widget().pack()
         self.canvas.draw()
-
+        
+        self.x = symbols('x') 
+        
+    # Plot + Calculation Function
     def plot (self):
         f= lambda x: eval(self.box.get())
+        ff= lambda x: self.box.get()
         x=np.linspace(float(self.boxa.get()), float(self.boxb.get()), 1001)
         pp=f(x)
+        p= ff(x)
+
+        D = diff(p, self.x) # Calcule du Dérivé
+        d = lambda x: eval(str(diff(p, self.x)))
+        I = integrate(p, self.x) # Calcule du Primitive
+        i = lambda x: eval(str(integrate(p, self.x)))
+
         self.a.cla()
         self.a.set_ylim([float(self.boxa.get()), float(self.boxb.get())])
         self.a.xaxis.set_ticks(np.arange(float(self.boxa.get()), float(self.boxb.get())+1, 1))
@@ -55,8 +93,20 @@ class mclass:
         self.a.set_ylabel("y=f(x)", fontsize=14)
         self.a.set_xlabel("x", fontsize=14)
         self.a.grid(True)
-        self.a.plot(x, f(x),color='blue')
+        
+        self.a.plot(x, pp,color='blue') # Afficher F(x)
+        self.a.plot(x, d(x),color='green') # Afficher F'(x)
+        self.a.plot(x, i(x),color='yellow') # Afficher F-1(x)
+        
+        
+        self.res_txt.set(D) # Afichage du Dérivé dans la GUI
+        self.resp_txt.set(I) # Afichage du Dérivé dans la GUI
+        
+        
+        
         self.canvas.draw()
+
+        
 
 
 window= Tk()
